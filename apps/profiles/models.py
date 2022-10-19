b8ba3f93e0d784a6bad3779d3626dadf0bb8337b
hamso <hamsonassor14@gmail.com>
2022-10-19 17:13:13 +0300

    add profile model and signals

--- apps/profiles/models.py
+++ apps/profiles/models.py
@@ -1,3 +1,53 @@
+from email.policy import default
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django_countries.fields import CountryField
+from phonenumber_field.modelfields import PhoneNumberField
+
+from apps.common.models import TimeStampedUUIDModel
 
 # Create your models here.
+
+User = get_user_model()
+
+
+class Gender(models.TextChoices):
+    MALE = "Male", _("Male")
+    FEMALE = "Female", _("Female")
+    OTHER = "Other", _("Other")
+
+
+class Profile(TimeStampedUUIDModel):
+    user = models.OneToOneField(User, related_name="profile", on_delete=models.CASCADE)
+    phone_number = PhoneNumberField(
+        verbose_name=_("Phone Number"), max_length=30, default="+254712345678"
+    )
+    about_me = models.TextField(
+        verbose_name=_("About me"), default="say something about yourself"
+    )
+    profile_photo = models.ImageField(
+        verbose_name=_("Profile Photo"), default="/profile_default.png"
+    )
+    gender = models.CharField(
+        verbose_name=_("Gender"),
+        choices=Gender.choices,
+        default=Gender.OTHER,
+        max_length=20,
+    )
+    country = CountryField(
+        verbose_name=_("Country"), default="KE", blank=False, null=False
+    )
+    city = models.CharField(
+        verbose_name=_("City"),
+        max_length=180,
+        default="Nairobi",
+        blank=False,
+        null=False,
+    )
+    site_manager = models.BooleanField(verbose_name = _("Site Manager"), default=False,help_text=_("Are you a Site Manager?"),)
+    meter_reader = models.BooleanField(verbose_name = _("Meter Reader"), default=False,help_text=_("Are you a Meter Reader?"),)
+    is_cutomer = models.BooleanField(verbose_name= _("Customer"), default=False,help_text=_("Are you a Customer?"),)
+
+    def __str__(self):
+        return f"{self.user.username}'s profile"
