53877c73fed769630e55d04d162e14da76d61e2e
hamso <hamsonassor14@gmail.com>
2022-10-26 11:44:20 +0300

    updated meter urls

--- apps/meters/urls.py
+++ apps/meters/urls.py
@@ -5,9 +5,15 @@ from . import views
 urlpatterns = [
     path("all/", views.ListAllMetersAPIView.as_view(), name="all-meters"),
     path(
-        "agents/", views.ListCustomersMetersAPIView.as_view(), name="customer-meters"
+        "customers/", views.ListCustomersMetersAPIView.as_view(), name="customer-meters"
     ),
     path("create/", views.create_meter_api_view, name="meter-create"),
+   path(
+        "details/<slug:slug>/",
+        views.MeterDetailView.as_view(),
+        name="property-details",
+    ),
+        
     path("update/<slug:slug>/", views.update_meter_api_view, name="update-meter"),
     path("delete/<slug:slug>/", views.delete_meter_api_view, name="delete-meter"),
     path("search/", views.MeterManagementSearchAPIView.as_view(), name="meter-search"),
