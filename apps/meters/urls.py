ed0b407232cf2c53caf3ca8402d54b18a6685b68
hamso <hamsonassor14@gmail.com>
2022-11-29 14:21:11 +0300

    meter resding urls

--- apps/meters/urls.py
+++ apps/meters/urls.py
@@ -8,14 +8,18 @@ urlpatterns = [
         "customers/", views.ListCustomersMetersAPIView.as_view(), name="customer-meters"
     ),
     path("create/", views.create_meter_api_view, name="meter-create"),
-   path(
-        "details/<slug:slug>/",
-        views.MeterDetailView.as_view(),
-        name="property-details",
-    ),
+    path(
+            "details/<slug:slug>/",
+            views.MeterDetailView.as_view(),
+            name="property-details",
+        ),
         
     path("update/<slug:slug>/", views.update_meter_api_view, name="update-meter"),
     path("delete/<slug:slug>/", views.delete_meter_api_view, name="delete-meter"),
     path("search/", views.MeterManagementSearchAPIView.as_view(), name="meter-search"),
+    path("create/", views.create_usage_rate_api_view, name="usage-rate-create"),
+    path("create/", views.create_unit_rate_api_view, name="unit-rate-create"),
+    path("create/", views.create_meter_reading_api_view, name="meter-reading-create"),
+    
 
 ]
