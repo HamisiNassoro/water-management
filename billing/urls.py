1fede0536fbd02d846d72dae466e25003c93326b
moses044 <moseswambui044@gmail.com>
2023-01-02 04:38:37 +0300

    Changing url files to use DefaultRouter For ease of use with Browsable API

--- billing/urls.py
+++ billing/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path,include
 
-from rest_framework.routers import SimpleRouter
+from rest_framework.routers import DefaultRouter
 from .views import PaymentViewSet
 
-router = SimpleRouter()
+router = DefaultRouter()
 router.register('payment', PaymentViewSet, 'payment')
 
 
